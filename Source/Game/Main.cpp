e28c963875256afe90ba5e81070e576163989d9d
KyuPang <vkdls754@gmail.com>
2022-03-21 23:42:51 +0900

    [LAB01] lab01 submitted (2022.03.21 11:42)

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,13 +1,73 @@
-﻿#include "Common.h"
+﻿/*+===================================================================
+  File:      MAIN.CPP
+  Summary:   This application demonstrates creating a Direct3D 11 device
+  Origin:    http://msdn.microsoft.com/en-us/library/windows/apps/ff729718.aspx
+  Originally created by Microsoft Corporation under MIT License
+  © 2022 Kyung Hee University
+===================================================================+*/
+#ifndef UNICODE
+#define UNICODE
+#endif // !UNICODE
+
+#include <Windows.h>
+
+#include "Common.h"
 
 #include "Game/Game.h"
 
 
 
+/*F+F+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  Function: wWinMain
+  Summary:  Entry point to the program. Initializes everything and
+            goes into a message processing loop. Idle time is used to
+            render the scene.
+  Args:     HINSTANCE hInstance
+              Handle to an instance.
+            HINSTANCE hPrevInstance
+              Has no meaning.
+            LPWSTR lpCmdLine
+              Contains the command-line arguments as a Unicode
+              string
+            INT nCmdShow
+              Flag that says whether the main application window
+              will be minimized, maximized, or shown normally
+  Returns:  INT
+              Status code.
+-----------------------------------------------------------------F-F*/
+
 
 INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ INT nCmdShow)
 {
-	library::PrintHi();
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	return 0; 
+
+    if (FAILED(library::InitWindow(hInstance, nCmdShow))) {
+        return 0;
+    }
+    if (FAILED(library::InitDevice())) {
+        return 0;
+    }
+
+    // Main message loop
+    MSG msg = { 0 };
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            library::Render();  // Do some rendering
+        }
+    }
+
+    // Destroy
+    library::CleanupDevice();
+
+    return static_cast<INT>(msg.wParam);
 }
+
--- Source/Library/Common.h
+++ Source/Library/Common.h
@@ -13,3 +13,15 @@
 #endif // ! UNICODE
 
 #include <windows.h>
+#include <wrl.h>
+
+#include <d3d11_4.h>
+#include <directxcolors.h>
+
+#include "Resource.h"
+
+
+
+constexpr LPCWSTR PSZ_COURSE_TITLE = L"Game Graphics Programming";
+
+using namespace DirectX;
